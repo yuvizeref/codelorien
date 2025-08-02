ad4700ef4072126e9ed3c29a8091fabe805099d1
Yuvraj <yuvizeref@gmail.com>
2025-08-02 14:35:16 +0000

    Configurable frontend server port.

--- frontend/src/middleware/axiosInstance.js
+++ frontend/src/middleware/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:8000/api/",
+  baseURL: `${import.meta.env.VITE_BACKEND_URL}/api`,
 });
 
 axiosInstance.interceptors.request.use(
